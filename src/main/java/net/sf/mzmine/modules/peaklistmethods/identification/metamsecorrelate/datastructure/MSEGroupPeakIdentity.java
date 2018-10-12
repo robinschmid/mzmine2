e52ebab4cfe2574333d06b099890df17d9a56fb7
Robin Schmid <robinschmid@uni-muenster.de>
2018-10-11 17:39:24 -0700

    Export annotation nodes and edges

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/metamsecorrelate/datastructure/MSEGroupPeakIdentity.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/metamsecorrelate/datastructure/MSEGroupPeakIdentity.java
@@ -1,21 +1,36 @@
 package net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.datastructure;
 
+import net.sf.mzmine.datamodel.PeakIdentity;
 import net.sf.mzmine.datamodel.PeakListRow;
-import net.sf.mzmine.datamodel.RawDataFile;
 import net.sf.mzmine.datamodel.impl.SimplePeakIdentity;
-import net.sf.mzmine.modules.peaklistmethods.identification.mascot.MascotSearchTask;
-import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.MetaMSEcorrelateTask;
 
 public class MSEGroupPeakIdentity extends SimplePeakIdentity {
-	private PKLRowGroup group;
-	
-	public MSEGroupPeakIdentity(PKLRowGroup group) { 
-		this.group = group;
-		int i = group.getGroupID();
-		setPropertyValue(PROPERTY_NAME, "G("+ (i<100? (i<10? "00" : "0") : "") +i+")");
-	} 
+  private PKLRowGroup group;
 
-	public PKLRowGroup getGroup() {
-		return group;
-	} 
+  public MSEGroupPeakIdentity(PKLRowGroup group) {
+    this.group = group;
+    int i = group.getGroupID();
+    setPropertyValue(PROPERTY_NAME, "G(" + (i < 100 ? (i < 10 ? "00" : "0") : "") + i + ")");
+  }
+
+  public PKLRowGroup getGroup() {
+    return group;
+  }
+
+  /**
+   * Finds the first group identity
+   * 
+   * @param r
+   * @return
+   */
+  public static MSEGroupPeakIdentity getIdentityOf(PeakListRow r) {
+    if (r == null)
+      return null;
+
+    for (PeakIdentity pi : r.getPeakIdentities()) {
+      if (pi instanceof MSEGroupPeakIdentity)
+        return (MSEGroupPeakIdentity) pi;
+    }
+    return null;
+  }
 }
--- src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/metamsecorrelate/export/ExportCorrAnnotationTask.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/metamsecorrelate/export/ExportCorrAnnotationTask.java
@@ -31,6 +31,7 @@ import net.sf.mzmine.datamodel.PeakIdentity;
 import net.sf.mzmine.datamodel.PeakList;
 import net.sf.mzmine.datamodel.PeakListRow;
 import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.MetaMSEcorrelateModule;
+import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.datastructure.MSEGroupPeakIdentity;
 import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.datastructure.MSEGroupedPeakList;
 import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.datastructure.R2RCorrMap;
 import net.sf.mzmine.modules.peaklistmethods.identification.metamsecorrelate.datastructure.R2RCorrelationData;
@@ -50,7 +51,7 @@ public class ExportCorrAnnotationTask extends AbstractTask {
   private static final Logger LOG = Logger.getLogger(ExportCorrAnnotationTask.class.getName());
 
   public enum ANNOTATION {
-    ID1, ID2, ANNOTATION1, ANNOTATION2, ANNO_NETID, MZ1, MZ2, DELTA_MZ, DELTA_AVG_RT, RT1, RT2, INTENSITY, AREA, AVG_CORR, AVG_DP, CORRELATED_F2F, TOTAL_CORR, IMAX_CORR;
+    ID1, ID2, ANNOTATION1, ANNOTATION2, ANNO_NETID, MZ1, MZ2, DELTA_MZ, DELTA_AVG_RT, RT1, RT2, INTENSITY1, INTENSITY2, AREA1, AREA2, CORR_GROUP, AVG_CORR, AVG_DP, CORRELATED_F2F, TOTAL_CORR, IMAX_CORR;
     private String header;
 
     ANNOTATION() {
@@ -58,6 +59,24 @@ public class ExportCorrAnnotationTask extends AbstractTask {
     }
 
     ANNOTATION(String header) {
+      this.header = header;
+    }
+
+    @Override
+    public String toString() {
+      return header != null ? header : super.toString();
+    }
+  }
+
+  public enum NODES {
+    ID, ANNOTATION, ANNO_NETID, MZ, RT, INTENSITY, AREA, CORR_GROUP;
+    private String header;
+
+    NODES() {
+      this.header = toString().replaceAll("_", " ");
+    }
+
+    NODES(String header) {
       this.header = header;
     }
 
@@ -137,8 +156,13 @@ public class ExportCorrAnnotationTask extends AbstractTask {
 
       // export annotation networks to file
       if (exAnnotationsFile) {
+        // edges file
         ANNOTATION[] ann = createColumns(exMZ, exDMZ, exDRT, exRT, exI, exArea, false);
-        exportAnnotationsToCSV(pkl, filename, progress, ann, this);
+        exportAnnotationsToCSV(pkl, corrMap, filename, progress, ann, this);
+
+        // nodes file
+        NODES[] ann2 = NODES.values();
+        exportAnnotationNodesToCSV(pkl, corrMap, filename, progress, ann2, this);
       }
       // export r2rcorr map to file
       if (exAvgCorrFile) {
@@ -189,12 +213,19 @@ public class ExportCorrAnnotationTask extends AbstractTask {
       ann.add(ANNOTATION.RT1);
       ann.add(ANNOTATION.RT2);
     }
-    if (exI)
-      ann.add(ANNOTATION.INTENSITY);
-    if (exArea)
-      ann.add(ANNOTATION.AREA);
+    if (exI) {
+      ann.add(ANNOTATION.INTENSITY1);
+      ann.add(ANNOTATION.INTENSITY2);
+    }
+    if (exArea) {
+      ann.add(ANNOTATION.AREA1);
+      ann.add(ANNOTATION.AREA2);
+    }
+
+    ann.add(ANNOTATION.CORR_GROUP);
+    ann.add(ANNOTATION.AVG_CORR);
+
     if (exCorr) {
-      ann.add(ANNOTATION.AVG_CORR);
       ann.add(ANNOTATION.AVG_DP);
       ann.add(ANNOTATION.CORRELATED_F2F);
       ann.add(ANNOTATION.TOTAL_CORR);
@@ -203,8 +234,127 @@ public class ExportCorrAnnotationTask extends AbstractTask {
     return ann.toArray(new ANNOTATION[ann.size()]);
   }
 
-  public static boolean exportAnnotationsToCSV(MSEGroupedPeakList pkl, File filename,
-      Double progress, ANNOTATION[] col, AbstractTask task) {
+
+  /**
+   * Exports all PeakListRows with their favored annotation
+   * 
+   * @param pkl
+   * @param corrMap can be null
+   * @param filename
+   * @param progress
+   * @param col
+   * @param task can be null
+   * @return
+   */
+  public static boolean exportAnnotationNodesToCSV(MSEGroupedPeakList pkl, R2RCorrMap corrMap,
+      File filename, Double progress, NODES[] col, AbstractTask task) {
+    try {
+      PeakListRow[] rows = pkl.getRows();
+      Arrays.sort(rows, new PeakListRowSorter(SortingProperty.ID, SortingDirection.Ascending));
+      int totalRows = rows.length;
+
+      StringBuilder ann = new StringBuilder();
+
+      // add header
+      ann.append(StringUtils.join(col, ','));
+      ann.append("\n");
+
+      int added = 0;
+      // for all rows
+      for (PeakListRow r : rows) {
+        if (task != null && task.isCanceled()) {
+          return false;
+        }
+
+        int rowID = r.getID();
+
+        PeakIdentity pi = r.getPreferredPeakIdentity();
+        // identity by ms annotation module
+        if (pi instanceof ESIAdductIdentity) {
+          ESIAdductIdentity id = (ESIAdductIdentity) pi;
+
+          // the data
+          Object[] data = new Object[col.length];
+
+          // add all data
+          for (int d = 0; d < col.length; d++) {
+            switch (col[d]) {
+              case ANNOTATION:
+                data[d] = id != null ? id.getAdduct() : null;
+                break;
+              case ANNO_NETID:
+                data[d] = id != null ? id.getNetID() : null;
+                break;
+              case ID:
+                data[d] = r.getID();
+                break;
+              case MZ:
+                data[d] = r.getAverageMZ();
+                break;
+              case AREA:
+                data[d] = r.getBestPeak().getArea();
+                break;
+              case INTENSITY:
+                data[d] = r.getBestPeak().getHeight();
+                break;
+              case RT:
+                data[d] = r.getAverageRT();
+                break;
+              case CORR_GROUP:
+                MSEGroupPeakIdentity gid = MSEGroupPeakIdentity.getIdentityOf(r);
+                if (gid != null)
+                  data[d] = gid.getGroup().getGroupID();
+                break;
+            }
+          }
+          // replace null
+          for (int j = 0; j < data.length; j++) {
+            if (data[j] == null)
+              data[j] = "";
+          }
+          // add data
+          ann.append(StringUtils.join(data, ','));
+          ann.append("\n");
+          added++;
+        }
+        progress += 1.0 / totalRows;
+      }
+      LOG.info("ALl data created for export. Total of " + added + " adduct nodes");
+
+      // export ann
+      // Filename
+      TxtWriter writer = new TxtWriter();
+      File realFile = FileAndPathUtil.eraseFormat(filename);
+      realFile = FileAndPathUtil.getRealFilePath(filename.getParentFile(),
+          realFile.getName() + "_annotation_nodes", ".csv");
+      writer.openNewFileOutput(realFile);
+      writer.write(ann.toString());
+      writer.closeDatOutput();
+      LOG.info("File created: " + realFile);
+      return true;
+    } catch (Exception t) {
+      LOG.log(Level.SEVERE, "Export of MS annotation nodes results error", t);
+      if (task != null) {
+        task.setStatus(TaskStatus.ERROR);
+        task.setErrorMessage(t.getMessage());
+      }
+      return false;
+    }
+  }
+
+  /**
+   * Creates Annotations edge file with all annotation links in this file
+   * 
+   * @param pkl
+   * @param corrMap can be null
+   * @param filename
+   * @param progress
+   * @param col
+   * @param task can be null
+   * @return
+   */
+  public static boolean exportAnnotationsToCSV(MSEGroupedPeakList pkl, R2RCorrMap corrMap,
+      File filename, Double progress, ANNOTATION[] col, AbstractTask task) {
     try {
       PeakListRow[] rows = pkl.getRows();
       Arrays.sort(rows, new PeakListRowSorter(SortingProperty.ID, SortingDirection.Ascending));
@@ -238,6 +388,9 @@ public class ExportCorrAnnotationTask extends AbstractTask {
                   // the data
                   Object[] data = new Object[col.length];
                   ESIAdductIdentity id = null;
+                  // r2r correlation
+                  R2RCorrelationData r2r = corrMap != null ? corrMap.get(r, link) : null;
+
                   // add all data
                   for (int d = 0; d < col.length; d++) {
                     switch (col[d]) {
@@ -271,6 +424,33 @@ public class ExportCorrAnnotationTask extends AbstractTask {
                       case MZ2:
                         data[d] = link.getAverageMZ();
                         break;
+                      case AREA1:
+                        data[d] = r.getBestPeak().getArea();
+                        break;
+                      case AREA2:
+                        data[d] = link.getBestPeak().getArea();
+                        break;
+                      case INTENSITY1:
+                        data[d] = r.getBestPeak().getHeight();
+                        break;
+                      case INTENSITY2:
+                        data[d] = link.getBestPeak().getHeight();
+                        break;
+                      case RT1:
+                        data[d] = r.getAverageRT();
+                        break;
+                      case RT2:
+                        data[d] = link.getAverageRT();
+                        break;
+                      case CORR_GROUP:
+                        MSEGroupPeakIdentity gid = MSEGroupPeakIdentity.getIdentityOf(r);
+                        if (gid != null)
+                          data[d] = gid.getGroup().getGroupID();
+                        break;
+                      case AVG_CORR:
+                        if (r2r != null && r2r.hasFeatureShapeCorrelation())
+                          data[d] = r2r.getAvgPeakShapeR();
+                        break;
                     }
                   }
                   // replace null
@@ -290,13 +470,13 @@ public class ExportCorrAnnotationTask extends AbstractTask {
         }
       }
 
-
       LOG.info("ALl data created for export. Total of " + added + " links");
       // export ann
       // Filename
       TxtWriter writer = new TxtWriter();
-      File realFile = FileAndPathUtil.getRealFilePath(filename.getParentFile(),
-          filename.getName() + "_annotations", ".csv");
+      File realFile = FileAndPathUtil.eraseFormat(filename);
+      realFile = FileAndPathUtil.getRealFilePath(filename.getParentFile(),
+          realFile.getName() + "_annotations", ".csv");
       writer.openNewFileOutput(realFile);
       writer.write(ann.toString());
       writer.closeDatOutput();
@@ -383,19 +563,46 @@ public class ExportCorrAnnotationTask extends AbstractTask {
                 data[d] = link.getAverageMZ();
                 break;
               case IMAX_CORR:
-                data[d] = r2r.getCorrIProfile().getR();
+                if (r2r.hasIMaxCorr())
+                  data[d] = r2r.getCorrIProfile().getR();
                 break;
               case TOTAL_CORR:
-                data[d] = r2r.getTotalCorrelation().getR();
+                if (r2r.hasFeatureShapeCorrelation())
+                  data[d] = r2r.getTotalCorrelation().getR();
                 break;
               case AVG_CORR:
-                data[d] = r2r.getAvgPeakShapeR();
+                if (r2r.hasFeatureShapeCorrelation())
+                  data[d] = r2r.getAvgPeakShapeR();
                 break;
               case CORRELATED_F2F:
-                data[d] = r2r.getCorrPeakShape().size();
+                if (r2r.hasFeatureShapeCorrelation())
+                  data[d] = r2r.getCorrPeakShape().size();
+                break;
+              case CORR_GROUP:
+                MSEGroupPeakIdentity pi = MSEGroupPeakIdentity.getIdentityOf(r);
+                if (pi != null)
+                  data[d] = pi.getGroup().getGroupID();
                 break;
               case AVG_DP:
                 data[d] = r2r.getAvgDPcount();
+                break;
+              case AREA1:
+                data[d] = r.getBestPeak().getArea();
+                break;
+              case AREA2:
+                data[d] = link.getBestPeak().getArea();
+                break;
+              case INTENSITY1:
+                data[d] = r.getBestPeak().getHeight();
+                break;
+              case INTENSITY2:
+                data[d] = link.getBestPeak().getHeight();
+                break;
+              case RT1:
+                data[d] = r.getAverageRT();
+                break;
+              case RT2:
+                data[d] = link.getAverageRT();
                 break;
             }
           }
@@ -415,8 +622,9 @@ public class ExportCorrAnnotationTask extends AbstractTask {
       // export ann
       // Filename
       TxtWriter writer = new TxtWriter();
-      File realFile = FileAndPathUtil.getRealFilePath(filename.getParentFile(),
-          filename.getName() + "_correlations", ".csv");
+      File realFile = FileAndPathUtil.eraseFormat(filename);
+      realFile = FileAndPathUtil.getRealFilePath(filename.getParentFile(),
+          realFile.getName() + "_correlations", ".csv");
       writer.openNewFileOutput(realFile);
       writer.write(ann.toString());
       writer.closeDatOutput();
