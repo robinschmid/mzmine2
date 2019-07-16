4e4c156dec3fd70fc32edbe4420f537bddbe45d1
Robin Schmid <robinschmid@uni-muenster.de>
2019-07-16 11:23:23 +0200

    Merged SpectralLibrarySubmissionWindow

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/io/spectraldbsubmit/view/SpectralLibrarySubmissionWindow.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/io/spectraldbsubmit/view/SpectralLibrarySubmissionWindow.java
@@ -131,7 +131,7 @@ public class SpectralLibrarySubmissionWindow extends JFrame {
   private String helpID;
 
   //
-  private boolean isFragmentScan = false;
+  private boolean isFragmentScan = true;
   // data either rows or list of entries with 1 or multiple scans
   private PeakListRow[] rows;
   private List<Scan[]> scanList;
@@ -262,6 +262,7 @@ public class SpectralLibrarySubmissionWindow extends JFrame {
    * @param raw
    */
   public void setData(PeakListRow[] rows, boolean isFragmentScan) {
+    getMSLevelComponent().setText(isFragmentScan ? "2" : "1");
     scanList = null;
     this.rows = rows;
     this.pnScanSelect = new ScanSelectPanel[rows.length];
@@ -373,7 +374,7 @@ public class SpectralLibrarySubmissionWindow extends JFrame {
     // disable gnps
     if (!isFragmentScan) {
       paramSubmit.getParameter(LibrarySubmitParameters.SUBMIT_GNPS).setValue(false);
-      getGnpsSubmitComponent().setSelected(false);;
+      getGnpsSubmitComponent().setSelected(false);
     }
     getGnpsSubmitComponent().setEnabled(isFragmentScan);
   }
@@ -666,13 +667,13 @@ public class SpectralLibrarySubmissionWindow extends JFrame {
         group.resetZoom();
     });
 
-    JMenuItem setSize = new JMenuItem("chart size");
-    menu.add(setSize);
-    setSize.addActionListener(e -> {
-      Dimension dim = SizeSelectDialog.getSizeInput();
-      if (dim != null)
-        setChartSize(dim);
-    });
+    // JMenuItem setSize = new JMenuItem("chart size");
+    // menu.add(setSize);
+    // setSize.addActionListener(e -> {
+    // Dimension dim = SizeSelectDialog.getSizeInput();
+    // if (dim != null)
+    // setChartSize(dim);
+    // });
 
     //
     addCheckBox(settings, "show legend", showLegend,
